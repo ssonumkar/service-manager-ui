7196b20548145de07f1e881ae6906e5783ca29d4
ssonumkar <moreanjani022@gmail.com>
2025-07-17 23:05:28 +0200

    First commit

---
+++ src/app/features/service-list/service-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-service-list',
+  imports: [],
+  templateUrl: './service-list.html',
+  styleUrl: './service-list.scss'
+})
+export class ServiceList {
+
+}
