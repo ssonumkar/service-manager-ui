0e75b480f9da60a8863fdd1bdfeb16a39a1e479f
sagar <Sagar.Sonumkar@iwhalecloud.com>
2025-07-19 20:17:32 +0200

    Updated the app to use the Navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,13 +6,14 @@ import { ResourceListComponent } from './features/resource-list/resource-list';
 import { OwnerFormComponent } from './features/owner-form/owner-form';
 import { OwnerListComponent } from './features/owner-list/owner-list';
 export const routes: Routes = [
-    {path: '', component: ServiceListComponent},
+    { path: '', redirectTo: 'services', pathMatch: 'full' },
+    {path: 'services', component: ServiceListComponent},
     {path: 'service/new', component: ServiceFormComponent},
     {path: 'service/:id/edit', component: ServiceFormComponent},
     {path: 'service/:id/resources', component: ResourceListComponent},
     {path: 'resource/new', component: ResourceFormComponent},
-    {path: 'resource/:id/edit', component: ResourceFormComponent},
+    {path: 'resource/:id/edit', component: ResourceListComponent},
     {path: 'resource/:id/owners', component: OwnerListComponent},
     {path: 'owner/new', component: OwnerFormComponent},
-    {path: 'owner/:id/edit', component: OwnerFormComponent}
+    {path: 'owner/:id/edit', component: OwnerListComponent}
 ];
